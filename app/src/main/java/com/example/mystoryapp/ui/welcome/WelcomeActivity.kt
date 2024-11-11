2acb08be6adc653ce3f1f107359d4ec1536e9d8d
rendra-p <rendraprasetya32@gmail.com>
2024-11-11 13:57:12 +0700

    Ui build error signup

---
+++ app/src/main/java/com/example/mystoryapp/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,29 @@
+package com.example.mystoryapp.ui.welcome
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mystoryapp.R
+import com.example.mystoryapp.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_signup)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
